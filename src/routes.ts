07ca4d77aab8fc38803d9db429b9b49210be8333
Caitano <josias.m.caitano@accenture.com>
2024-03-18 11:04:10 -0300

    refactor: remoove controller videos

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { SubjectController } from "./controllers/subject_controller";
 import { RoomController } from "./controllers/room.controller";
-import { VideoController } from "./controllers/video.controller";
 
 const routes = Router();
 
