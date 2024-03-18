e8cb00a0933f3ac9656c173bd9f2cfd727b44186
Caitano <josias.m.caitano@accenture.com>
2024-03-18 12:15:02 -0300

    fix: fixed database

--- src/config/db/data-source.ts
+++ src/config/db/data-source.ts
@@ -11,6 +11,6 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    entities: [`./**/entities/*.{ts,js}`],
-    migrations: [`./**/migrations/*.{ts,js}`],
+    entities: [`../../**/entities/*.{ts,js}`],
+    migrations: [`../../**/migrations/*.{ts,js}`],
 })
